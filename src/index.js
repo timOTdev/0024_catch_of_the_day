14b847f5e45944cec3486b3b4857e67f409e7f2d
Timothy <timh1203@gmail.com>
2017-12-14 15:27:44 -0500

    Add react router dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
+import './css/style.css';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
-import './css/style.css';
+import NotFound from './components/NotFound';
+
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+    <div>
+      <Route exact path="/" component={StorePicker} />
+      <Route path="/about" component={App} />
+      <Route path="" component={NotFound} />
+    </div>
+  </BrowserRouter>
+  )
+}
 
 render (
-  <App />,
+  <Root />,
   document.querySelector("#main")
 )
