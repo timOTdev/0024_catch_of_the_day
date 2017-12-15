7ea4dfa02112266cf9baed292b2a3b2d8a7b08bb
Timothy <timh1203@gmail.com>
2017-12-15 14:28:05 -0500

    Add fish form

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Inventory from './Inventory';
 class App extends React.Component {
   render() {
     return (
-      <div className="cath-of-the-day">
+      <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './css/style.css';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
@@ -9,12 +9,12 @@ import NotFound from './components/NotFound';
 
 const Root = () => {
   return (
-    <BrowserRouter>
+  <BrowserRouter>
     <div>
       <Switch>
-      <Route exact path="/" component={StorePicker} />
-      <Route path="/about" component={App} />
-      <Route component={NotFound} />
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/about" component={App} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   </BrowserRouter>
