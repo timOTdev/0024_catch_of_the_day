e5419909ad66b81066bf417d32a2b02eef366725
Timothy <timh1203@gmail.com>
2017-12-15 15:14:44 -0500

    Add render of sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes.js'
+import Fish from './Fish';
 
 class App extends React.Component {
   constructor() {
@@ -31,6 +32,11 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          {
+            Object
+              .keys(this.state.fishes)
+              .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+          }
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
