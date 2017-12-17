3c77b8edd6afa44a33198449e492ca95eed5115b
Timothy <timh1203@gmail.com>
2017-12-17 17:17:20 -0500

    Add editing of fishes and live update

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     this.state = {
@@ -23,6 +24,13 @@ class App extends React.Component {
     fishes[`fish-${timeStamp}`] = fish
     this.setState({ fishes });
   }
+
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
 
   loadSamples() {
     this.setState({ fishes: sampleFishes })
@@ -45,8 +53,17 @@ class App extends React.Component {
               .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
           }
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Order 
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params={this.props.params}
+        />
+        <Inventory 
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     )
   }
